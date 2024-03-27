a1704f10c1ba134742008194f0c19e71bebbda7d
Himsara-Edirisingha <87166862+Himsara-Edirisingha@users.noreply.github.com>
2024-03-27 23:11:24 +0530

    securing routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter,Routes, Route, Navigate } from "react-router-dom";
 import MapIndex from './scenes/weather_data/index'
+import Login from './scenes/user/login'
+import { Dashboard } from '@mui/icons-material';
+
 
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsAuthenticated(!!token);
+  }, []);
+
   return (
-    <div>
-      <MapIndex />
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={isAuthenticated ? (<Dashboard />) : (<Navigate replace to={"/login"} />)} />
+      <Route path="/" element={<MapIndex />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 export default App
