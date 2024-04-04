b6bf91c099a762e9fd0479f9210dde01585f1bb1
Himsara-Edirisingha <87166862+Himsara-Edirisingha@users.noreply.github.com>
2024-04-04 21:29:23 +0530

    sweet alert 2 with weather data

--- src/scenes/weather_data/index.jsx
+++ src/scenes/weather_data/index.jsx
@@ -3,7 +3,10 @@ import { MapContainer, Polygon, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import { geoData } from '../../data/geoData'
 import Box from '@mui/material/Box'
-import swal from 'sweetalert';
+import swal from 'sweetalert2';
+import rainy from '../../assests/life-winter.gif'
+import snowy from '../../assests/snow-winter.gif'
+import sunny from '../../assests/sunnyday.gif'
 
 
 const center = [7.688843, 80.665844]
@@ -24,10 +27,29 @@ const MapIndex = () => {
     fetchData();
     const interval = setInterval(fetchData, 10000);
     return () => clearInterval(interval);
-   
+
   }, []);
 
- // console.log('Rendered apiData:', apiData);
+  // console.log('Rendered apiData:', apiData);
+
+  async function getBackgroundGif(temperature, humidity, airPressure) {
+    if (temperature > 30 && humidity > 70 && airPressure < 1000) {
+      return `url(${sunny})`;
+
+    }else if(temperature < 0){
+      return `url(${snowy})`
+    } 
+    else if(temperature > 32){
+      return `url(${sunny})`
+    } 
+    else if (temperature < 10 && humidity < 30 && airPressure > 1000) {
+      return `url(${snowy})`;
+    } else {
+      return `url(${rainy})`;
+    }
+  }
+
+
 
   return (
     <MapContainer
@@ -78,13 +100,34 @@ const MapIndex = () => {
                     color: "white",
                   });
                 },
-                click: (e) => {
-                
-                  swal({
+                click: async (e) => {
+                  const { temperature, humidity, airPressure } = apiData.find(dataitem => dataitem.StatName === feature.properties.electoralDistrictCode) || {};
+                  const backgroundGif = await getBackgroundGif(temperature, humidity, airPressure);
+                  swal.fire({
                     title: feature.properties.electoralDistrict,
-                    text:  JSON.stringify(apiData.find((dataitem)=>dataitem.StatName === feature.properties.electoralDistrictCode))
-                  
+                    width: 600,
+                    padding: "3em",
+                    color: "#716add",
+                    background: "#fff url(/images/trees.png)",
+                    backdrop: `
+                            rgba(0, 0, 123, 0.4)
+                            ${backgroundGif}
+                            left top
+                            no-repeat
+                            `,
+                    html: `
+                         <ul >
+                         <li>Temperature: ${temperature || 'N/A'}</li>
+                         <li>Humidity: ${humidity || 'N/A'}</li>
+                         <li>Air Pressure: ${airPressure || 'N/A'}</li>
+                         </ul>
+                        `
                   });
+                  // swal.fire({
+                  //   title: feature.properties.electoralDistrict,
+                  //   text:  JSON.stringify(apiData.find((dataitem)=>dataitem.StatName === feature.properties.electoralDistrictCode))
+
+                  // });
                 }
               }}
             />
