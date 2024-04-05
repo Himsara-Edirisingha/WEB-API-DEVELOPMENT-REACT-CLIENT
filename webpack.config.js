2fd428707efe8636745bd0f1f378885fb4306f33
Himsara-Edirisingha <87166862+Himsara-Edirisingha@users.noreply.github.com>
2024-04-05 15:06:36 +0530

    route Config for Live Server

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const history = require('connect-history-api-fallback'); 
+
+module.exports = {
+  entry: "./src/App.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"], 
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"], 
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: ["file-loader"], 
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({ template: "./public/index.html" }),
+  ],
+  devServer: {
+    historyApiFallback: true, 
+  },
+};
